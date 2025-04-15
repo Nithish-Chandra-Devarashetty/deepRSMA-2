f40dafda1bfcf19c8a10a49b0d162f735c07a974
nithishchandra7 <nithishchandra16@gmail.com>
2025-04-15 18:59:24 +0530

    Fix RNA_feature_extraction initialization in DeepRSMA class by adding missing hidden_channels parameter

--- main_independent.py
+++ main_independent.py
@@ -56,7 +56,7 @@ class CustomDualDataset(Dataset):
         return self.dataset1[index], self.dataset2[index]
 
     def __len__(self):
-        return len(self.dataset1)  
+        return len(self.dataset1)
 
 
 
@@ -72,8 +72,8 @@ class DeepRSMA(nn.Module):
     def __init__(self):
         super(DeepRSMA, self).__init__()
         # RNA graph + seq
-        self.rna_graph_model = RNA_feature_extraction(hidden_dim)
-        
+        self.rna_graph_model = RNA_feature_extraction(hidden_dim, hidden_dim)
+
         # Mole graph
         self.mole_graph_model = GNN_molecule(hidden_dim)
         # Mole seq
@@ -81,27 +81,27 @@ class DeepRSMA(nn.Module):
 
         # Cross fusion module
         self.cross_attention = cross_attention(hidden_dim)
-        
+
         self.line1 = nn.Linear(hidden_dim*2, 1024)
         self.line2 = nn.Linear(1024, 512)
         self.line3 = nn.Linear(512, 1)
         self.dropout = nn.Dropout(0.2)
-        
+
         self.rna1 = nn.Linear(hidden_dim, hidden_dim*4)
         self.mole1 = nn.Linear(hidden_dim, hidden_dim*4)
-        
+
         self.rna2 = nn.Linear(hidden_dim*4, hidden_dim)
         self.mole2 = nn.Linear(hidden_dim*4, hidden_dim)
-        
+
         self.relu = nn.ReLU()
-    
+
     def forward(self, rna_batch, mole_batch):
         rna_out_seq,rna_out_graph, rna_mask_seq, rna_mask_graph, rna_seq_final, rna_graph_final = self.rna_graph_model(rna_batch, device)
-        
+
         mole_graph_emb, mole_graph_final = self.mole_graph_model(mole_batch)
-        
+
         mole_seq_emb, _, mole_mask_seq = self.mole_seq_model(mole_batch, device)
-        
+
         mole_seq_final = (mole_seq_emb[-1]*(mole_mask_seq.to(device).unsqueeze(dim=2))).mean(dim=1).squeeze(dim=1)
 
 
@@ -119,34 +119,34 @@ class DeepRSMA(nn.Module):
             flag += count_i
         mole_out_graph = torch.stack(mole_out_graph).to(device)
         mole_mask_graph = torch.tensor(mask, dtype=torch.float)
-        
+
         context_layer, attention_score = self.cross_attention([rna_out_seq, rna_out_graph, mole_seq_emb[-1], mole_out_graph], [rna_mask_seq.to(device), rna_mask_graph.to(device), mole_mask_seq.to(device), mole_mask_graph.to(device)], device)
 
-        
+
         out_rna = context_layer[-1][0]
         out_mole = context_layer[-1][1]
-        
+
         # Affinity Prediction Module
         rna_cross_seq = ((out_rna[:, 0:512]*(rna_mask_seq.to(device).unsqueeze(dim=2))).mean(dim=1).squeeze(dim=1) + rna_seq_final ) / 2
-        rna_cross_stru = ((out_rna[:, 512:]*(rna_mask_graph.to(device).unsqueeze(dim=2))).mean(dim=1).squeeze(dim=1) + rna_graph_final) / 2        
+        rna_cross_stru = ((out_rna[:, 512:]*(rna_mask_graph.to(device).unsqueeze(dim=2))).mean(dim=1).squeeze(dim=1) + rna_graph_final) / 2
 
         rna_cross = (rna_cross_seq + rna_cross_stru) / 2
         rna_cross = self.rna2(self.dropout((self.relu(self.rna1(rna_cross)))))
 
-        
+
         mole_cross_seq = ((out_mole[:,0:128]*(mole_mask_seq.to(device).unsqueeze(dim=2))).mean(dim=1).squeeze(dim=1) + mole_seq_final) / 2
         mole_cross_stru = ((out_mole[:,128:]*(mole_mask_graph.to(device).unsqueeze(dim=2))).mean(dim=1).squeeze(dim=1) + mole_graph_final) / 2
-        
+
         mole_cross = (mole_cross_seq + mole_cross_stru) / 2
-        mole_cross = self.mole2(self.dropout((self.relu(self.mole1(mole_cross)))))   
-        
+        mole_cross = self.mole2(self.dropout((self.relu(self.mole1(mole_cross)))))
+
         out = torch.cat((rna_cross, mole_cross),1)
         out = self.line1(out)
         out = self.dropout(self.relu(out))
         out = self.line2(out)
         out = self.dropout(self.relu(out))
         out = self.line3(out)
-        
+
 
         return out
 
@@ -183,7 +183,7 @@ for epoch in range(0,EPOCH):
         pre = model(batch[0].to(device), batch[1].to(device))
 
         y = batch[0].y
-        
+
         loss = loss_fct(pre.squeeze(dim=1), y.float())
         loss.backward()
         optimizer.step()
@@ -211,9 +211,9 @@ for epoch in range(0,EPOCH):
             max_p = p[0]
             print(' ')
             print('Best:', 'epo:',epoch, 'pcc:',p[0],'scc: ',s[0],'rmse:',rmse)
-            
+
             os.makedirs('save', exist_ok=True)
             torch.save(model.state_dict(), 'save/' + 'model_independent_'+str(seed)+'.pth')
 
-        
+
         model.train()
